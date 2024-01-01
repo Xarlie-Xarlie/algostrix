06518e50f898203e2f45e596814fde7a5f053a7f
charliecharlie <jeancharles552@gmail.com>
2024-01-01 17:04:58 -0300

    feat: impl hash table

---
+++ lib/algostrix/data_structures/hash_table.ex
@@ -0,0 +1,44 @@
+defmodule AlgoStrix.HashTable do
+  @moduledoc """
+  Hash Tables are one of the most commom data structes.
+
+  You use it to store a colection of data with key and values.
+
+  It deals with hash colisions. 
+
+  Hash Table operations and complexity:
+    - loolup: O(1)
+    - push: O(1)
+    - insert: O(1)
+    - delete: O(1)
+  """
+
+  defstruct table: %{}
+
+  def new, do: %__MODULE__{}
+
+  def set(%__MODULE__{table: table} = hash_table, {key, value}) do
+    hash = :erlang.phash2(key)
+
+    case Map.get(table, hash) do
+      nil -> [{key, value}]
+      values -> [{key, value} | values]
+    end
+    |> then(&Map.put(table, hash, &1))
+    |> then(&%{hash_table | table: &1})
+  end
+
+  def get(%__MODULE__{table: table}, key) do
+    hash = :erlang.phash2(key)
+    Map.get(table, hash)
+  end
+
+  def keys(%__MODULE__{} = hash_table) do
+    Enum.map(hash_table.table, fn {_, values} ->
+      values
+    end)
+    |> List.flatten()
+    |> Enum.map(fn {k, _v} -> k end)
+    |> Enum.uniq()
+  end
+end
